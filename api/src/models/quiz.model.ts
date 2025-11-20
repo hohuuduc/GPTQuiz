5065324a2a1e55eee11cb002f742fa1e58563d25
hohuuduc <hohuuduc2709@gmail.com>
2025-11-20 23:57:44 +0700

    feat: Add OAuth2 server configuration and Quiz Mongoose model.

--- api/src/models/quiz.model.ts
+++ api/src/models/quiz.model.ts
@@ -1,5 +1,5 @@
 import { Schema, model, Document } from 'mongoose';
-const { v4: uuidv4 } = require('uuid');
+import { v4 as uuidv4 } from 'uuid';
 
 export interface IQuiz extends Document {
   content: string;
